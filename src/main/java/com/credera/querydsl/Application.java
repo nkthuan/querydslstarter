4fc3ee0802000cc19b8d7474c9108291c7a33e04
Bryan Chapman <bryan.chapman@epsilon.com>
2014-05-13 16:08:45 -0500

    Update to use transaction service and filter based on authenticated user

--- src/main/java/com/credera/querydsl/Application.java
+++ src/main/java/com/credera/querydsl/Application.java
@@ -16,7 +16,7 @@ public class Application {
         //setup repositories
         BranchLocationRepository branchRepository = context.getBean(BranchLocationRepository.class);
         CustomerRepository customerRepository = context.getBean(CustomerRepository.class);
-        TransactionRepository transactionRepository = context.getBean(TransactionRepository.class);
+        TransactionService transactionService = new TransactionService(context.getBean(TransactionRepository.class));
         
         //seed some data
         new SeedData(context).seedData();
@@ -44,7 +44,7 @@ public class Application {
         
         // fetch all transactions
         QTransaction tr = QTransaction.transaction;
-        Iterable<Transaction> transactions = transactionRepository.findAll(tr.customer.username.eq(UserService.getCurrentUser()));
+        Iterable<Transaction> transactions = transactionService.findAll();
         System.out.println("Transactions found with findAll():");
         System.out.println("-------------------------------");
         for (Transaction transaction : transactions) {
@@ -52,8 +52,8 @@ public class Application {
         }
         System.out.println();
         
-        // fetch all transctions for Bank of Texas
-        transactions = transactionRepository.findAll(tr.branchLocation.bankName.eq("Bank of Texas").and(tr.customer.username.eq(UserService.getCurrentUser())));
+        // fetch all transactions for Bank of Texas
+        transactions = transactionService.findAll(tr.branchLocation.bankName.eq("Bank of Texas"));
         System.out.println("Transactions found for \"Bank of Texas\" locations:");
         System.out.println("-------------------------------");
         for (Transaction transaction : transactions) {
@@ -61,8 +61,8 @@ public class Application {
         }
         System.out.println();
 
-        // fetch all transctions over $10,000
-        transactions = transactionRepository.findAll(tr.transactionAmount.gt(10000).and(tr.customer.username.eq(UserService.getCurrentUser())));
+        // fetch all transactions over $10,000
+        transactions = transactionService.findAll(tr.transactionAmount.gt(10000));
         System.out.println("Transactions found with amount over $10,000:");
         System.out.println("-------------------------------");
         for (Transaction transaction : transactions) {
@@ -71,7 +71,7 @@ public class Application {
         System.out.println();
         
         // fetch all Alice's transactions
-        transactions = transactionRepository.findAll(tr.customer.username.eq("alain").and(tr.customer.username.eq(UserService.getCurrentUser())));
+        transactions = transactionService.findAll(tr.customer.username.eq("alain"));
         System.out.println("Transactions found for Alice:");
         System.out.println("-------------------------------");
         for (Transaction transaction : transactions) {
